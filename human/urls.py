d754ff90dba3494def19763ad1943d45d9a12bb2
antonnifo <antonnifo@live.com>
2023-02-13 13:27:53 +0300

    add url pattern 2 map edit view

--- human/urls.py
+++ human/urls.py
@@ -5,7 +5,9 @@ from . import views
 
 urlpatterns = [
         path('', include('django.contrib.auth.urls')),
-        path('dashboard/', views.dashboard, name="dashboard")
+        path('dashboard/', views.dashboard, name="dashboard"),
+        path('register/', views.register, name='register'),
+        path('edit/', views.edit, name='edit'),
 ]
 
 
