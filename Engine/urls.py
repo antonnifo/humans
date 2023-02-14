05b89e39fd5697996af065c77f5f6e7b86a99b77
antonnifo <antonnifo@live.com>
2023-02-14 10:37:04 +0300

    add social authentification url

--- Engine/urls.py
+++ Engine/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('human.urls')),
+    path('social-auth/',
+            include('social_django.urls', namespace='social')),
 ]
