2d53b82e4b1f74c5a587dcc42303d3fd851ff454
Mwangi  Anthony <antonnifo@live.com>
2023-04-13 16:45:39 +0300

    add raw id field for user

--- human/admin.py
+++ human/admin.py
@@ -4,5 +4,6 @@ from .models import Profile
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'date_of_birth', 'photo']
+    raw_id_fields = ['user']
     
    
