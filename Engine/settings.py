59f8de483e85ef8a1aeb6b2a238e4fc26640ea7d
Mwangi  Anthony <antonnifo@live.com>
2023-04-14 16:15:27 +0300

    add function to create user profile

--- Engine/settings.py
+++ Engine/settings.py
@@ -154,3 +154,16 @@ SOCIAL_AUTH_TWITTER_SECRET = 'XXX' # Twitter API Secret
 
 SOCIAL_AUTH_FACEBOOK_KEY = 'XXX' # Facebook App ID
 SOCIAL_AUTH_FACEBOOK_SECRET = 'XXX' # Facebook App Secret
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
