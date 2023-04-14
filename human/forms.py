d0c92a9dc23404fc1537bc64aece285e8d10f615
Mwangi  Anthony <antonnifo@live.com>
2023-04-14 10:45:02 +0300

    add validation for  email field
    Prevent users from changing their existing email address to an existing email address of another user and multiple users from registering with the same email address

--- human/forms.py
+++ human/forms.py
@@ -17,6 +17,12 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
+    
+     def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError('Email already in use.')
+        return data   
 
 
 class UserEditForm(forms.ModelForm):
@@ -24,10 +30,17 @@ class UserEditForm(forms.ModelForm):
         model = User
         fields = ('first_name', 'last_name', 'email')
 
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        qs = User.objects.exclude(id=self.instance.id)\
+                         .filter(email=data)
+        if qs.exists():
+            raise forms.ValidationError('Email already in use.')
+        return data        
+
         
 class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('date_of_birth', 'photo')
-
 
